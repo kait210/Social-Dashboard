ee38d99c1215b282f200dba68b05b37c8013ae48
Matt <mattpgough@gmail.com>
2016-03-25 10:32:45 +0000

    Implemented Gruntfile to set environmental variables and hide keys. Adjusted e2e to account for extra email verification.

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -2,24 +2,23 @@
 
 describe('Social Dashboard', function() {
 
-
   it('should be able to authenticate a Twitter account', function() {
     browser.get('index.html');
     element(by.id('twitter-auth')).click();
 
     browser.getAllWindowHandles().then(function (handles) {
-    browser.switchTo().window(handles[1]);
-    browser.driver.findElement(By.id('username_or_email')).sendKeys('dashboardronin');
-    browser.driver.findElement(By.id('password')).sendKeys('dashboardteam');
-    browser.driver.findElement(By.id('allow')).click();
-    browser.driver.findElement(By.id('challenge_response')).sendKeys('');
-    browser.driver.findElement(By.id('email_challenge_submit')).click();
-    browser.switchTo().window(handles[0]);});
+      browser.switchTo().window(handles[1]);
+      browser.driver.findElement(By.id('username_or_email')).sendKeys('dashboardronin');
+      browser.driver.findElement(By.id('password')).sendKeys('dashboardteam');
+      browser.driver.findElement(By.id('allow')).click();
 
-    browser.sleep(6000);
-
+      if (browser.driver.findElements(By.id('challenge_response')).length != 0) {
+        browser.driver.findElement(By.id('challenge_response')).sendKeys('socialdashboardronin@gmail.com');
+        browser.driver.findElement(By.id('email_challenge_submit')).click();
+      }
+      browser.switchTo().window(handles[0]);
+    });
+    browser.sleep(2000);
     expect(element(by.id('alertMessage')).getText()).toEqual('Twitter authentication successful!')
-  });
-
-
+   });
 });
