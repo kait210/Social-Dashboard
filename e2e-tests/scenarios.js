b5f542a9c19ea14b2a3ca3c4db27896bf4592b0d
Matthew Drake <matdrake@live.co.uk>
2016-03-25 11:23:51 +0000

    Added if/else to Twitter auth test to handle varying authentication requirements

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -15,8 +15,10 @@ describe('Social Dashboard', function() {
       if (browser.driver.findElements(By.id('challenge_response')).length != 0) {
         browser.driver.findElement(By.id('challenge_response')).sendKeys('socialdashboardronin@gmail.com');
         browser.driver.findElement(By.id('email_challenge_submit')).click();
+        browser.switchTo().window(handles[0]);
+      } else {
+        browser.switchTo().window(handles[0]);
       }
-      browser.switchTo().window(handles[0]);
     });
     browser.sleep(2000);
     expect(element(by.id('alertMessage')).getText()).toEqual('Twitter authentication successful!')
